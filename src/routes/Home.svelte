ac5e3dac6685f14b272072825d8c606c2e4d7398
RehaanAnjariaAgain <rehaan1099@gmail.com>
2024-10-09 17:24:30 -0400

    added become a mentor button to home page

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -65,6 +65,11 @@
                     Find a mentor<ArrowRightOutline size="md" class="ml-2 -mr-1" />
                 </Button>
             </a>
+            <a href="/becomeamentor">
+                <Button size="xl" color="yellow" class="inline-flex items-center justify-center mr-3">
+                    Become a mentor<ArrowRightOutline size="md" class="ml-2 -mr-1" />
+                </Button>
+            </a>
         {:else}
             <a href="/auth/signup">
                 <Button size="xl" color="green" class="inline-flex items-center justify-center mr-3">
