b23f146f8633ea1670077897678057e9b2524d0a
RehaanAnjariaAgain <rehaan1099@gmail.com>
2024-12-08 12:48:40 -0500

    show site is down

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { inject } from '@vercel/analytics';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import { Alert } from "flowbite-svelte"
 
 	// Adds dynamic title to each page, paired with svelte:head below
 	const appName = 'CRLS PathFinders';
@@ -19,6 +20,14 @@
 
 <div class="mainlayoutwrapper bg-gray-100" style="height:100%;">
 	<MainNav></MainNav>
+
+	<center>
+		<Alert color="red" border>
+			<span class="font-medium">SITE IS DOWN</span>
+			<br>
+			CRLS PathFinders is down for 24 hours - sorry for the inconvenience :(
+		</Alert>
+	</center>
 
 	<slot></slot>
 </div>
