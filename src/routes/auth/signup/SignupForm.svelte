f5c8fd255275fc24c3bfa45034cf4bd32cd37385
RehaanAnjariaAgain <rehaan1099@gmail.com>
2025-01-09 15:16:02 -0500

    fixed signup bug

--- src/routes/auth/signup/SignupForm.svelte
+++ src/routes/auth/signup/SignupForm.svelte
@@ -13,28 +13,28 @@
 	import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
 
 	// Send email confirmation:
-	const actionCodeSettings = {
-		// URL you want to redirect back to. The domain (www.example.com) for this
-		// URL must be in the authorized domains list in the Firebase Console.
-		url: 'https://www.crlspathfinders.com/finishSignUp?cartId=1234',
-		// This must be true.
-		handleCodeInApp: true
-	};
+	// const actionCodeSettings = {
+	// 	// URL you want to redirect back to. The domain (www.example.com) for this
+	// 	// URL must be in the authorized domains list in the Firebase Console.
+	// 	url: 'https://www.crlspathfinders.com/finishSignUp?cartId=1234',
+	// 	// This must be true.
+	// 	handleCodeInApp: true
+	// };
 
-	const ok_auth = getAuth();
-	sendSignInLinkToEmail(ok_auth, email, actionCodeSettings)
-		.then(() => {
-			// The link was successfully sent. Inform the user.
-			// Save the email locally so you don't need to ask the user for it again
-			// if they open the link on the same device.
-			window.localStorage.setItem('emailForSignIn', email);
-			// ...
-		})
-		.catch((error) => {
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			// ...
-		});	
+	// const ok_auth = getAuth();
+	// sendSignInLinkToEmail(ok_auth, email, actionCodeSettings)
+	// 	.then(() => {
+	// 		// The link was successfully sent. Inform the user.
+	// 		// Save the email locally so you don't need to ask the user for it again
+	// 		// if they open the link on the same device.
+	// 		window.localStorage.setItem('emailForSignIn', email);
+	// 		// ...
+	// 	})
+	// 	.catch((error) => {
+	// 		const errorCode = error.code;
+	// 		const errorMessage = error.message;
+	// 		// ...
+	// 	});	
 
 	const SEND_URL = import.meta.env.VITE_URL;
 
